5434297284d008f32b95856f529c731ac10946b4
Rahulc-t <rahulct500@gmail>
2024-09-08 18:58:57 +0530

    modified2

--- api/[id].js
+++ api/[id].js
@@ -1,4 +1,4 @@
-const MetaData =require("./Metadata.json");
+const MetaData =require("../Metadata.json");
 module.exports=(req,res)=>{
     const {id}=req.query;
     res.json(JSON.parse(JSON.stringify(MetaData[id])))
